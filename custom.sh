feb4480d4576e252e2ee4df4da0d44d35bb7fde3
MaYuming <mayuming@aliyun.com>
2018-10-26 23:42:59 +0800

    del grepagent

--- custom.sh
+++ custom.sh
@@ -1,4 +1,7 @@
 alias vi=vim
+#filter ctags and cscope symbols
+alias grep='grep --color --exclude="tags" --exclude="cscope.out" --exclude="cscope.in.out"'
 PATH=${PATH}:/opt/custom_tool/
 
+#custom PS1
 PS1="\u@$(hostname -I | awk '{print $1}') \W ->"
